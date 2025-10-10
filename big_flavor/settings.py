f09dc29ae01cb021dc56e1bbd72bf83f0dbec845
NDOMBAXIBONGA <jobdealmeida016@gmail.com>
2025-10-10 11:39:17 +0100

    Abilitando o DEBUG no Settings

--- big_flavor/settings.py
+++ big_flavor/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = config('SECRET_KEY', default='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
