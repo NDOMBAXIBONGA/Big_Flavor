2c19b08cb61dbe128fbecb4917323a2392d09f15
NDOMBAXIBONGA <jobdealmeida016@gmail.com>
2025-10-17 10:40:40 +0100

    Adicionando o SQL LITE Nativo do Django

--- big_flavor/settings.py
+++ big_flavor/settings.py
@@ -127,12 +127,12 @@ if os.getenv('RAILWAY'):
 }
 else:
     # DESENVOLVIMENTO (Local)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
-        }
+   DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
