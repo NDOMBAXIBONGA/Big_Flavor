6f30564ed07af276ffc91dcda0a32c5586b364df
NDOMBAXIBONGA <jobdealmeida016@gmail.com>
2025-10-17 14:54:09 +0100

    Adicionando Dependencias

--- big_flavor/settings.py
+++ big_flavor/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 
     'blog',
     'carinho',
@@ -66,6 +67,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
