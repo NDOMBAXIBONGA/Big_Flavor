ec13f7d8a3b7e42ab1cd4ad3267248ea1b02009d
NDOMBAXIBONGA <jobdealmeida016@gmail.com>
2025-10-26 21:01:38 +0000

    Ayalizando o HTML

--- big_flavor/settings.py
+++ big_flavor/settings.py
@@ -237,7 +237,7 @@ AUTH_USER_MODEL = 'conta.Usuario'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # Devo comentar em Desenvolvimento
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' # Devo comnetar em Producao
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage' # Devo comentar em Desenvolvimento
 
 # Media files
