6ab3559a7e5a7abb19a208e893996f8d356245e4
NDOMBAXIBONGA <jobdealmeida016@gmail.com>
2025-10-29 14:41:33 +0000

    Atualizando o HTML Parte 8

--- big_flavor/settings.py
+++ big_flavor/settings.py
@@ -62,7 +62,19 @@ INSTALLED_APPS = [
     'index',
     'menu',
     'sobre',
+
+    'storages',
 ]
+
+# Configuração AWS S3
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
 
 # settings.py
 MIDDLEWARE = [
@@ -210,5 +222,14 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' # 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage' # Devo comentar em Desenvolvimento
 
 # Media files
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+# Storage settings
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Se quiser static files também no S3:
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+# URLs
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
